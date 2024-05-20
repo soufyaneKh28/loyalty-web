8b693f5b752ac4b8902e1c66dd86b2adc1d6e4b5
soufyane khalfallah <khalfalasoufi@gmail.com>
2024-05-20 13:25:44 +0300

    local-remove

--- src/app/Local.js
+++ src/app/Local.js
@@ -1,9 +1,9 @@
-"use client";
+// "use client";
 
-import { useLocale } from "next/router";
-import { Children } from "react";
+// import { useLocale } from "next/router";
+// import { Children } from "react";
 
-export default function Local({ children }) {
-  const locale = useLocale();
-  return { Children({locale})};
-}
+// export default function Local({ children }) {
+//   const locale = useLocale();
+//   return { Children({locale})};
+// }
