53d94db5b47817fe477efbdeae0d662bf1601929
soufyane khalfallah <khalfalasoufi@gmail.com>
2024-06-27 16:11:59 +0300

    re-adding button comp

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ children }) => {
+  return (
+    <button className="w-fit text-primary rtl:font-black text-[14px] font-medium mt-4  bg-secondary rounded-full px-9 py-3 hover:px-12 transition-all hover:bg-amber-500">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
